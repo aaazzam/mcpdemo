d6f2c072d9402737fd60a7a3d82e37d0ddb39cb1
Adam Azzam <33043305+AAAZZAM@users.noreply.github.com>
2025-06-10 18:53:50 -0400

    Update main.py

--- demo/main.py
+++ demo/main.py
@@ -5,12 +5,6 @@ mcp = FastMCP("Demofffffffffffff!fffffffffasfaffsfsfffffff!fffffffff!!! 🚀")
 
 
 @mcp.tool()
-def add(a: int, b: int) -> int:
-    """Add two numbers."""
-    return a + b
-
-
-@mcp.tool()
 def subtract(a: int, b: int) -> int:
     """Subtract two numbers"""
     return a - b
