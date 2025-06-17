2b3d76a57d5e80ddc06faf73a6565ebd443deb9c
Adam Azzam <33043305+AAAZZAM@users.noreply.github.com>
2025-06-17 12:26:59 -0400

    Update main.py

--- demo/main.py
+++ demo/main.py
@@ -1,7 +1,7 @@
 # server.py
 from fastmcp import FastMCP
 
-mcp = FastMCP("Demoff!fffffafffaffsfsfffffff!fffffffff!!! 🚀")
+mcp = FastMCP("Demoff!fffffafffaffsfsfffffff!ffffffffff!!! 🚀")
 
 
 @mcp.tool()
