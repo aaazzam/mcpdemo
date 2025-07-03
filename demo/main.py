6b828c9d341b3196069b579f2120698f2bea209b
Adam Azzam <33043305+AAAZZAM@users.noreply.github.com>
2025-07-03 13:03:37 -0400

    Update main.py

--- demo/main.py
+++ demo/main.py
@@ -1,8 +1,31 @@
 # server.py
 from fastmcp import FastMCP
 import math
+from dataclasses import dataclass
+from datetime import datetime
+from fastmcp.context import Context
 
 mcp = FastMCP("Demo! 🚀")
+
+
+@dataclass
+class Person:
+    name: str
+
+
+@mcp.tool
+async def ask_for_name(context: Context) -> str:
+    """Ask for user's name using elicitation"""
+    print("before elicit")
+    result = await context.elicit(
+        message="What is your name?",
+        response_type=Person,
+    )
+    print("after elicit")
+    if result.action == "accept":
+        return f"Hello, {result.data.name}!"
+    else:
+        return "No name provided."
 
 
 @mcp.tool()
