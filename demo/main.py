7ab7115a545899a564ff63a71e1a94e88c9b3c6a
Adam Azzam <33043305+AAAZZAM@users.noreply.github.com>
2025-07-04 17:03:30 -0400

    Update main.py

--- demo/main.py
+++ demo/main.py
@@ -11,15 +11,3 @@ mcp = FastMCP("Demo!!!! 🚀")
 def multiply(a: int, b: int) -> int:
     """Multiply two numbers"""
     return a * b
-
-
-@mcp.tool()
-def add(a: int, b: int) -> int:
-    """Add two numbers"""
-    return a + b
-
-
-@mcp.tool()
-def square(a: int) -> int:
-    """Square a number"""
-    return a * a
